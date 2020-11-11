bebdfae4c164f5506d1ac3e6973ece901e59e534
Oshiro <otmn1067@gmail.com>
2020-11-11 11:03:13 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-card>
+    <v-card width="400px" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="display-1">ログイン</h1>
       </v-card-title>
@@ -8,9 +8,9 @@
     <v-card-text>
       <v-form>
         <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
-        <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
+        <v-text-field v-bind:type="showPassword ? 'text' : 'password'" prepend-icon="mdi-lock" v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" label="パスワード" @click:append="showPassword = !showPassword" />
         <v-card-actions>
-          <v-btn>ログイン</v-btn>
+          <v-btn class="info">ログイン</v-btn>
         </v-card-actions>
 
       </v-form>
@@ -23,7 +23,7 @@
 export default {
   name: 'App',
   data: () => ({
-    //
+    showPassword : false
   }),
   
 };
